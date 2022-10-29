72b0fdcc258853dd343755425a41961548fd5f98
heatwaven <markopavlovic205051@gmail.com>
2022-10-29 21:04:01 +0200

    working on server development

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,28 +2,28 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        User u1 = new User("User1");
-        User u2 = new User("User2");
-
-        u1.buyPackage(4);
-        u2.buyPackage(4);
-
-        u1.openPackages();
-        u2.openPackages();
-
-        User winner = Battle.battle(u1, u2);
-
-
-        if (winner == null) {
-            System.out.println("No winner");
-        } else {
-            System.out.println("Der Gewinner ist " + winner.getUsername());
-        }
-
-
-//        for (var card : u1.getCards()) {
-//            System.out.println("card: " + card.getClass().getSimpleName());
+//        User u1 = new User("User1");
+//        User u2 = new User("User2");
+//
+//        u1.buyPackage(4);
+//        u2.buyPackage(4);
+//
+//        u1.openPackages();
+//        u2.openPackages();
+//
+//        User winner = Battle.battle(u1, u2);
+//
+//
+//        if (winner == null) {
+//            System.out.println("No winner");
+//        } else {
+//            System.out.println("Der Gewinner ist " + winner.getUsername());
 //        }
+//
+
+
+
+
 
     }
 }
--- src/main/java/main/rest/App.java
+++ src/main/java/main/rest/App.java
@@ -1,30 +1,4 @@
 package main.rest;
 
-import lombok.AccessLevel;
-import lombok.Setter;
-
 public class App {
-//    @Setter(AccessLevel.PRIVATE)
-//    private CityController cityController;
-//
-//    public App() {
-//        setCityController(new CityController(new CityService()));
-//    }
-//
-//    public Response handleRequest(Request request) {
-//
-//
-//        switch (request.getMethod()) {
-//            case GET: {
-//                if (request.getPathname().equals("/cities")) {
-//                    return this.cityController.getCities();
-//                } else {
-//
-//                    String cityId = request.getPathname().split("/")[2];
-//                    return this.cityController.getCityById(Integer.parseInt(cityId));
-//                }
-//            }
-//        }
-//
-//        return new Response(HttpStatus.NOT_FOUND, ContentType.JSON, "{ \"error\": \"Not Found\", \"data\": null }");
-    }
+}
